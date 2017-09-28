939d893413e445fc78825827fc0211df8b57e9de
Michael Cretzman <michael.cretzman@kaazing.com>
2017-09-28 15:11:26 -0700

    Update KaazingJMSDemoPage.cs
    updating demo server, demos.kaazing.com

--- jms/KaazingJMSXamarinDemo.Common/KaazingJMSDemoPage.cs
+++ jms/KaazingJMSXamarinDemo.Common/KaazingJMSDemoPage.cs
@@ -40,7 +40,7 @@ namespace KaazingJMSXamarinDemo
             uriEntry = new Entry {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand,
-                Text = "wss://sandbox.kaazing.net/jms",
+                Text = "wss://demos.kaazing.com/jms",
             };
 
             uriEntry.Focus();
